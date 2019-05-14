73abecb569f7c2c5477bdc4559999593943fadd1
James Dorr <jamesdorr3@gmail.com>
2019-05-14 13:55:26 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,23 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return state.concat(action.quote)
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE': // mutates state
+      let upvoteQuote = {...state.find(quote => quote.id === action.quoteId)}
+      upvoteQuote.votes++
+      const newState = state.filter(quote => quote.id !== action.quoteId)
+      return [...newState, upvoteQuote]
+    case 'DOWNVOTE_QUOTE':
+      let downvoteQuote = {...state.find(quote => quote.id === action.quoteId)}
+      downvoteQuote.votes > 0 ? downvoteQuote.votes-- : null
+      const stateWithoutDownvote = state.filter(quote => quote.id !== action.quoteId)
+      return [...stateWithoutDownvote, downvoteQuote]
+    default:
+      return state;
+  }
 }
